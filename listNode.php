66c349a08bf9589dfbc71a40f8dd1af544317d07
lulu1417 <43202244+lulu1417@users.noreply.github.com>
2019-08-05 16:09:12 +0800

    object example

--- listNode.php
+++ listNode.php
@@ -2,7 +2,7 @@
 class ListNode {
     public $val;
     public $next = NULL;
-    public function __construct(int $val,ListNode $node=null) { // =null -> default value
+    public function __construct(int $val,ListNode $node=null) { // =null -> default value, if !parameter, then set as null
         $this->val = $val;
         $this->next = $node;
     }
