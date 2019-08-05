ecd27b5408383156eede5e9987b5cba3e287d74a
lulu1417 <43202244+lulu1417@users.noreply.github.com>
2019-08-05 16:07:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require 'listNode.php';
 require 'linkedList.php';
 require 'recursion.php';
-$c = new ListNode(3, new ListNode(2, new ListNode(1))); //constructor
+$c = new ListNode(3, new ListNode(2, new ListNode(1)));
     while($c!=null) {
         echo $c->val."\n";
         $c= $c->next;
