d0e4aa309f0edf03d7393cc8e1fbc226280309ae
lulu1417 <43202244+lulu1417@users.noreply.github.com>
2019-08-05 16:08:26 +0800

    Update listNode.php

--- listNode.php
+++ listNode.php
@@ -2,7 +2,7 @@
 class ListNode {
     public $val;
     public $next = NULL;
-    public function __construct(int $val,ListNode $node=null) { // ?__?
+    public function __construct(int $val,ListNode $node=null) { // =null -> default value
         $this->val = $val;
         $this->next = $node;
     }
