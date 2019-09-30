b533a0ce1d9574bc7ecffb304334efdfb0bf5676
lulu1417 <43202244+lulu1417@users.noreply.github.com>
2019-09-30 20:34:25 +0800

    Create removeDuplicates.php

---
+++ removeDuplicates.php
@@ -0,0 +1,16 @@
+ class Solution{
+   function removeDuplicates(&$nums) {
+        $n = count($nums); $count = 1; $k = $nums[0];
+        for($i=1;$i<$n;$i++){
+             if($nums[$i]>$k){
+                $nums[$count] = $nums[$i];
+                 $count++;
+                 $k = $nums[$i];
+             }                
+           
+        }
+       return $count;
+    
+    }
+ }
+ 
