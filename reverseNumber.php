5cf336e9f5c2846e0c847eab5d2d0c91314a2379
lulu1417 <boa1417@gmail.com>
2020-04-17 15:54:16 +0800

    reverse integer

--- reverseNumber.php
+++ reverseNumber.php
@@ -1,24 +1,17 @@
 <?php
 const MAX = 2147483647;
 const MIN = -2147483648;
-$x = 214748347;
+$x = -214748364;
 $num_length = strlen((string)$x);
 $result = 0;
 if ($x < 0) {
     $num_length -= 1;
 }
-$i = 0;
-if ($x % 10 == 0) {
-    $i = 1;
-    $num_length++;
-}
-
-while ($i < $num_length) {
+for ($i = 0; $i < $num_length; $i++) {
     $num = $x % 10;
     $add = $num * pow(10, $num_length - $i - 1);
     $result += $add;
     $x = ($x - $num) / 10;
-    $i++;
 }
 if ($result > MAX || $result < MIN) {
     $result = 0;
