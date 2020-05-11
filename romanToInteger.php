779cbc7bd3e8270e8973995aefa574d0e0c6eb09
lulu1417 <boa1417@gmail.com>
2020-05-11 16:21:58 +0800

    roman to integer

--- romanToInteger.php
+++ romanToInteger.php
@@ -1,5 +1,5 @@
 <?php
-$s = '';
+$s = 'LVIII';
 $roman = [
     'I' => 1,
     'V' => 5,
@@ -9,4 +9,16 @@ $roman = [
     'D' => 500,
     'M' => 1000
 ];
+$sum = $num = $roman[$s[strlen($s)-1]];
+for ($i = strlen($s)-2; $i > -1; $i--) {
+    $prevNum = $num;
+    $num = $roman[$s[$i]];
+    if ($prevNum > $num) {
+        $sum -= $num;
+    } else {
+        $sum += $num;
+    }
+}
+echo $sum;
+
 
